36975871a061c116a400f58c7d5b268c7d09496b
Kirill Shklyarevsky <kirillmoloch@gmail.com>
2020-03-17 19:23:56 +0300

    Add test for class AmountOfSymbols

---
+++ DEV-1/DEV-1Tests/AmountOfSymbolsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DEV_1;
+using System;
+
+namespace DEV_1.Tests
+{
+    [TestClass()]
+    public class AmountOfSymbolsTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CheckExeptionThrowIfStringIsNull()
+        {
+            AmountOfSymbols testString = new AmountOfSymbols();
+            testString.AmountOfNonRepeatingSymbolsInARow(null);
+        }
+
+        [DataRow("", 0)]
+        [DataRow("abccefgh", 5)]
+        [DataRow("aaaaa", 1)]
+        [TestMethod()]
+        public void AmountOfNonRepeatingSymbolsInARowTest(string line, int expected)
+        {
+            AmountOfSymbols testString = new AmountOfSymbols();
+
+            int actual = testString.AmountOfNonRepeatingSymbolsInARow(line);
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
