386c926822c66977d2f8ff6112c8e4f63353a1b4
Kirill Shklyarevsky <kirillmoloch@gmail.com>
2020-03-17 18:52:42 +0300

    reworked whole class AmountOfSymbols

--- DEV-1/DEV-1/AmountOfSymbols.cs
+++ DEV-1/DEV-1/AmountOfSymbols.cs
@@ -1,12 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DEV_1
 {
-    class AmountOfSymbols
+    public class AmountOfSymbols
     {
+        public int AmountOfNonRepeatingSymbolsInARow(string line)
+        {
+            if (line == null)
+            {
+                throw new ArgumentException();
+            }
+
+            int amount = 0;
+            int maxAmount = 0;
+            char previousSymbol;
+
+            if (line != String.Empty)
+            {
+                previousSymbol = line[0];
+                foreach (char currentSymbol in line)
+                {
+                    if (currentSymbol != previousSymbol)
+                    {
+                        amount++;
+                        previousSymbol = currentSymbol;
+                    }
+                    else
+                    {
+                        amount = 1;
+                        previousSymbol = currentSymbol;
+                    }
+
+                    if (amount > maxAmount)
+                    {
+                        maxAmount = amount;
+                    }
+                }
+                return maxAmount;
+            }
+            return 0;
+        }
     }
 }
